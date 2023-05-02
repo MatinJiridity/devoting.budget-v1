cdea20dff6992277e879f5b53383e90fd6dfc6bc
MatinJiridity <104725671+MatinJiridity@users.noreply.github.com>
2023-05-02 12:11:27 +0300

    link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,27 @@
 import * as React from 'react';
-import { Link, Typography, Container, Box } from '@material-ui/core';
-import { GitHub } from '@material-ui/icons';
+import { Link, Typography, Container, Box, Avatar } from '@material-ui/core';
+import { GitHub, HowToVote } from '@material-ui/icons';
 
 function Copyright() {
   return (
+    <div>
+    <Typography variant="h5" color="text.secondary" align="center">
+      <HowToVote style={{width: '30px', height: '30px'}}/>
+      <Link style={{ margin: '10px' }} color="inherit" href="https://devoting-ballotbox.onrender.com/">
+        Cast a Vote
+      </Link>{' '}
+      {/* {new Date().getFullYear()}
+      {'.'} */}
+    </Typography>
     <Typography variant="body2" color="text.secondary" align="center">
       {'My Github '} <GitHub style={{ width: '20px', height: '20px' }} />
-      <Link style={{ margin: '10px' }} color="inherit" href="https://github.com/MatinJiridity/">
+      <Link style={{ margin: '10px' }} color="inherit" href="https://github.com/MatinJiridity/devoting.budget-v1/tree/master/">
         matinjiridity
       </Link>{' '}
       {/* {new Date().getFullYear()}
       {'.'} */}
     </Typography>
+    </div>
   );
 }
 
