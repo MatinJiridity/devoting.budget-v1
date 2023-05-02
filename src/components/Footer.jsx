ce39774eb2b2b9f615868da8276cb7c662e2afef
MatinJiridity <104725671+MatinJiridity@users.noreply.github.com>
2023-05-02 11:40:16 +0300

    footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import { GitHub } from '@material-ui/icons';
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'My Github '} <GitHub style={{ width: '20px', height: '20px' }}/>
-      <Link style={{margin: '10px'}} color="inherit" href="https://github.com/MatinJiridity/">
+      {'My Github '} <GitHub style={{ width: '20px', height: '20px' }} />
+      <Link style={{ margin: '10px' }} color="inherit" href="https://github.com/MatinJiridity/">
         matinjiridity
       </Link>{' '}
       {/* {new Date().getFullYear()}
@@ -19,22 +19,24 @@ function Footer(props) {
   const { description, title } = props;
 
   return (
-    <Box component="footer" style={{ marginTop:'60px', bgcolor: 'background.paper', py: 4 , borderRadius:'10px', maxHeight:'10px'}}>
-      <Container maxWidth="lg">
-        <Typography variant="h6" align="center" gutterBottom>
-          {title}
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          {description}
-        </Typography>
-        <Copyright />
-      </Container>
-    </Box>
+    <footer>
+      <Box component="footer" style={{ marginTop: '60px', bgcolor: 'background.paper', py: 4, borderRadius: '10px', maxHeight: '10px' }}>
+        <Container maxWidth="lg">
+          <Typography variant="h6" align="center" gutterBottom>
+            {title}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            {description}
+          </Typography>
+          <Copyright />
+        </Container>
+      </Box>
+    </footer>
   );
 }
 
